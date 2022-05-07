19164a2d954c5eff14536c3053125b478bd9be07
Bertie Spell <berniesnell@gmail.com>
2022-05-07 17:24:11 +0200

    Read IPFS data

---
+++ be-barrio/README.md
@@ -0,0 +1,9 @@
+This BE server provides a way to store
+
+Install dependencies
+
+`npm install`
+
+To start the development server:
+
+`npm run nodemon`
--- be-barrio/routes/ipfs.js
+++ be-barrio/routes/ipfs.js
@@ -1,72 +1,88 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var { Web3Storage, getFilesFromPath } = require('web3.storage');
+var {
+  Web3Storage,
+  getFilesFromPath,
+  makeStorageClient,
+} = require("web3.storage");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const storage = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN })
+const storage = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
 
 async function storeFilesInIpfs(req, res) {
-    try {
-        if(!req.files.listings) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            try {
-                let data = []; 
+  try {
+    if (!req.files.listings) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+      });
+    } else {
+      try {
+        let data = [];
 
-                for (const file of req.files.listings) {
-                    //move photo to uploads directory
-                    const path = './uploads/' + file.name
-                    file.mv(path);
+        for (const file of req.files.listings) {
+          // move photo to uploads directory
+          const path = "./uploads/" + file.name;
+          file.mv(path);
 
-                    const pathFiles = await getFilesFromPath(path)
-                    data.push(...pathFiles)
-                }
-
-                const cid = await storage.put(data)
-
-                res.send({
-                    status: true,
-                    message: 'Files are uploaded with cid:' + cid,
-                    data: data
-                });
-            } catch (err) {
-                console.log(err)
-                res.status(500).send(err);
-            }
-            
+          const pathFiles = await getFilesFromPath(path);
+          data.push(...pathFiles);
         }
-    } catch (err) {
+
+        const cid = await storage.put(data);
+
+        res.send({
+          status: true,
+          message: "Files are uploaded with cid:" + cid,
+          data: data,
+        });
+      } catch (err) {
         res.status(500).send(err);
+      }
     }
-}
-
-async function main () {
-  const storage = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN })
-  const files = []
-
-  for (const path of ["./test.png"]) {
-    const pathFiles = await getFilesFromPath(path)
-    files.push(...pathFiles)
+  } catch (err) {
+    res.status(500).send(err);
   }
-
-  console.log(`Uploading ${files.length} files`)
-  const cid = await storage.put(files)
-  console.log('Content added with CID:', cid)
-  return cid;
 }
 
-/* GET ipfs listing. */
-router.get('/', async function(req, res, next) {
-  const cid = await main();
-  res.send(cid);
+async function retrieveFilesFromIpfs(req, res) {
+  const cid = req.body.cid;
+
+  if (!cid) {
+    res.send({
+      status: false,
+      message: "No CID provided",
+    });
+  } else {
+    try {
+      const result = await storage.get(cid);
+      console.log(`Got a response! [${result.status}] ${result.statusText}`);
+      if (!result.ok) {
+        throw new Error(`failed to get ${cid}`);
+      }
+
+      // unpack File objects from the response
+      const files = await result.files();
+      for (const file of files) {
+        console.log(`${file.cid} -- ${file.name} -- ${file.size}`);
+      }
+      res.send(files);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+  }
+}
+
+/* Get a listing from ipfs */
+router.get("/", async function (req, res, next) {
+  await retrieveFilesFromIpfs(req, res);
 });
 
-router.post('/', async function(req, res, next) {
-    await storeFilesInIpfs(req, res);
+/* Store a new listing in ipfs */
+router.post("/", async function (req, res, next) {
+  await storeFilesInIpfs(req, res);
 });
 
 module.exports = router;
